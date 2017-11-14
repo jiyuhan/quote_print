16ba258ac958c9938904e4470a0aa9d2013ebaf7
jiyuhan <jiyu-han@uiowa.edu>
2017-11-14 02:33:53 -0600

    coolest thing ever done

--- node/index.js
+++ node/index.js
@@ -4,14 +4,27 @@ const port = 1201;
 const fs = require('fs')
 
 var server = http.createServer((req, res)=>{
-    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-    console.log(req.url);
-    fs.readFile('index.html', 'utf8', function (err,data) {
-      if (err) {
-        return console.log(err);
-      }
-      res.write(data);
-      res.end();
-    });
 
+    if(req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+        console.log(req.url);
+        fs.readFile('index.html', 'utf8', function (err,data) {
+          if (err) {
+            return console.log(err);
+          }
+          res.write(data);
+          res.end();
+        });
+    }
+    else {
+        res.writeHead(200, {'Content-Type': 'image/jpg'});
+        console.log(req.url);
+        fs.readFile(req.url.replace('/', ''), function (err,data) {
+          if (err) {
+            return console.log(err);
+          }
+          //res.write(data);
+          res.end(data);
+        });
+    }
 }).listen(port);
